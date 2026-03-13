a8e306f1a98ea06d4a3f0316314645ebc83c7a5e
liam-holland <liam.holland96@gmail.com>
2026-03-13 16:26:52 +0000

    Created functions to automate audio effects

--- Summative Assignment 2/audio_AML/audio_AML/test.py
+++ Summative Assignment 2/audio_AML/audio_AML/test.py
@@ -68,12 +68,26 @@ def _(joined_df):
 def _(pathlib, pb):
     def applyReverb(_filePath, _uuid4 , _folderOutput):
 
-        board = pb.Pedalboard( [pb.Reverb(room_size = 0.25)] )
+        # To understand where these chave come from help(pb.Reverb)
+        reverb = pb.Reverb(room_size = 0.5, 
+                           damping = 0.5, 
+                           wet_level = 0.33, 
+                           dry_level = 0.4, 
+                           width = 1.0, 
+                           freeze_mode = 0.0)
+
+        board = pb.Pedalboard([reverb])
 
         filePath = f".\\{str(pathlib.Path(_filePath))}"
-        folderOutput = f".\\{str(pathlib.Path(_folderOutput))}" 
-
-        pathOutput = f"{folderOutput}./REVERB_{_uuid4}.wav"
+    
+        #Output file path
+        output_dir = pathlib.Path(_folderOutput)
+    
+        # Create the folder if it's missing
+        output_dir.mkdir(parents=True, exist_ok=True)
+    
+        # Use / operator to join paths (handles slashes automatically)
+        pathOutput = str(output_dir / f"REVERB_{_uuid4}.wav")
 
         # Open an audio file for reading, just like a regular file:
         with pb.io.AudioFile(f'{filePath}') as f:
@@ -95,13 +109,131 @@ def _(pathlib, pb):
 
 
 @app.cell
-def _(applyReverb):
-    applyReverb(f"./Medley-solos-DB/Medley-solos-DB_test-0_003d41a8-afad-501f-fbc3-d406dc694ceb.wav","003d41a8-afad-501f-fbc3-d406dc694ceb","./reverb/")
+def _(pathlib, pb):
+    def applyDelay(_filePath, _uuid4 , _folderOutput):
+
+        # To understand where these chave come from help(pb.Reverb)
+        delay = pb.Delay(delay_seconds = 0.5, 
+                          feedback = 0.3, 
+                          mix = 0.5)
+
+        board = pb.Pedalboard([delay])
+
+        # Input file path
+        filePath = f".\\{str(pathlib.Path(_filePath))}"
+
+        #Output file path
+        output_dir = pathlib.Path(_folderOutput)
+    
+        # Create the folder if it's missing
+        output_dir.mkdir(parents=True, exist_ok=True)
+    
+        # Use / operator to join paths (handles slashes automatically)
+        pathOutput = str(output_dir / f"DELAY_{_uuid4}.wav")
+
+        # Open an audio file for reading, just like a regular file:
+        with pb.io.AudioFile(f'{filePath}') as f:
+      
+          # Open an audio file to write to:
+          with pb.io.AudioFile(pathOutput, 'w', f.samplerate, f.num_channels) as o:
+      
+            # Read one second of audio at a time, until the file is empty:
+            while f.tell() < f.frames:
+              chunk = f.read(f.samplerate)
+          
+              # Run the audio through our pedalboard:
+              effected = board(chunk, f.samplerate, reset=False)
+          
+              # Write the output to our output file:
+              o.write(effected)
+
+    return (applyDelay,)
+
+
+@app.cell
+def _(pathlib, pb):
+    def applyDistortion(_filePath, _uuid4 , _folderOutput):
+
+        # To understand where these chave come from help(pb.Reverb)
+        distortion = pb.Distortion(drive_db = 23.0)
+
+        board = pb.Pedalboard([distortion])
+
+        filePath = f".\\{str(pathlib.Path(_filePath))}"
+    
+        #Output file path
+        output_dir = pathlib.Path(_folderOutput)
+    
+        # Create the folder if it's missing
+        output_dir.mkdir(parents=True, exist_ok=True)
+    
+        # Use / operator to join paths (handles slashes automatically)
+        pathOutput = str(output_dir / f"DISTORTION_{_uuid4}.wav")
+
+        # Open an audio file for reading, just like a regular file:
+        with pb.io.AudioFile(f'{filePath}') as f:
+      
+          # Open an audio file to write to:
+          with pb.io.AudioFile(pathOutput, 'w', f.samplerate, f.num_channels) as o:
+      
+            # Read one second of audio at a time, until the file is empty:
+            while f.tell() < f.frames:
+              chunk = f.read(f.samplerate)
+          
+              # Run the audio through our pedalboard:
+              effected = board(chunk, f.samplerate, reset=False)
+          
+              # Write the output to our output file:
+              o.write(effected)
+
+    return (applyDistortion,)
+
+
+@app.cell
+def _(pathlib, pb):
+    def applyChorus(_filePath, _uuid4 , _folderOutput):
+
+        # To understand where these chave come from help(pb.Reverb)
+        chorus = pb.Chorus(rate_hz = 1.0, 
+                           depth = 0.25, 
+                           centre_delay_ms = 7.0, 
+                           feedback = 0.0, 
+                           mix = 0.5)
+
+        board = pb.Pedalboard([chorus])
+
+        filePath = f".\\{str(pathlib.Path(_filePath))}"
+    
+        #Output file path
+        output_dir = pathlib.Path(_folderOutput)
+    
+        # Create the folder if it's missing
+        output_dir.mkdir(parents=True, exist_ok=True)
+    
+        # Use / operator to join paths (handles slashes automatically)
+        pathOutput = str(output_dir / f"CHORUS_{_uuid4}.wav")
+
+        # Open an audio file for reading, just like a regular file:
+        with pb.io.AudioFile(f'{filePath}') as f:
+      
+          # Open an audio file to write to:
+          with pb.io.AudioFile(pathOutput, 'w', f.samplerate, f.num_channels) as o:
+      
+            # Read one second of audio at a time, until the file is empty:
+            while f.tell() < f.frames:
+              chunk = f.read(f.samplerate)
+          
+              # Run the audio through our pedalboard:
+              effected = board(chunk, f.samplerate, reset=False)
+          
+              # Write the output to our output file:
+              o.write(effected)
+
     return
 
 
 @app.cell
-def _(applyReverb, joined_df):
+def _(applyDelay, applyDistortion, applyReverb, joined_df):
     for i in range(20):
 
         # Use iloc to get integer based position
@@ -109,12 +241,16 @@ def _(applyReverb, joined_df):
         _id = str(joined_df["uuid4"].iloc[i])
     
         applyReverb(_path, _id, "reverb/")
+        applyDelay(_path, _id, "delay/")
+        applyDistortion(_path, _id, "distortion/")
+        applyDistortion(_path, _id, "chorus/")
+    
     return
 
 
 @app.cell
 def _(pb):
-    help(pb.Reverb)
+    help(pb.Chorus)
     return
 
 
